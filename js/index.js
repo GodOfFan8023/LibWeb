b1651ea74e5d9006933a54b02b04d17afcee17c4
fanjinquan8023 <1090475411@qq.com>
2018-09-03 08:47:27 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,4 @@
+/*状态提示*/
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
---
+++ js/video.js
@@ -0,0 +1,18 @@
+/*状态提示*/
+$(function() {
+	$('[data-toggle="tooltip"]').tooltip()
+})
+//视频播放功能
+// $(document).ready(function () {
+//     $(".tab-pane li").click(function () {
+//         var a = $(this).attr("value");
+//         var t = $(this).text();
+//         alert(t);
+//         $("#video").attr({
+//             "src" : a,
+//             "autoplays":autoplays
+//         });
+//         $("#video_title").text(t);
+//     });
+// });
+// //
